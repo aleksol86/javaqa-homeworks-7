5e8c57696b4a5c0871f0e1c4076e0a24733be94e
Aleksandr Olenin <aleksandr.olenin@mail.ru>
2020-02-28 23:39:16 +0300

    Добавлен java-код

---
+++ BonusMilesService.java
@@ -0,0 +1,8 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int priceTicket = 5200;
+        int priceBonusMile = 20;
+        int mileQuantity = 5200 / 20;
+        return 5200 / 20;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int priceTicket = 5200;
+        int priceBonusMile = 20;
+        int milesQuantity = 5200 / 20;
+        System.out.println(milesQuantity);
+    }
+
+}
